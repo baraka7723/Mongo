eb286a646b27d439472dac02d50921febd1aa671
Armin Shams <44529358+baraka7723@users.noreply.github.com>
2023-07-02 13:32:45 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -48,4 +48,4 @@ def select_documents():
         return jsonify(error='No documents found.'), 404
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=6000, debug=True)
+    app.run(host='0.0.0.0', port=6000, debug=True)
